5a966103d792fd1937c53c7aba5e73d4e3dd629d
zhou <123>
2024-10-18 16:58:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,150 @@
+<div align="center">
+
+<h1>IEBins: Iterative Elastic Bins for Monocular Depth Estimation</h1>
+
+<div>
+    <a href='https://scholar.google.com.hk/citations?hl=zh-CN&user=ecZHSVQAAAAJ' target='_blank'>Shuwei Shao</a><sup>1</sup>&emsp;
+    <a target='_blank'>Zhongcai Pei</a><sup>1</sup>&emsp;
+    <a target='_blank'>Xingming Wu</a><sup>1</sup>&emsp;
+    <a target='_blank'>Zhong Liu</a><sup>1</sup>&emsp;
+    <a href='https://scholar.google.com.hk/citations?hl=zh-CN&user=5PoZrcYAAAAJ' target='_blank'>Weihai Chen</a><sup>2</sup>&emsp;
+    <a href='https://scholar.google.com.hk/citations?hl=zh-CN&user=LiUX7WQAAAAJ' target='_blank'>Zhengguo Li</a><sup>3</sup>
+</div>
+<div>
+    <sup>1</sup>Beihang University, <sup>2</sup>Anhui University, <sup>3</sup>A*STAR
+</div>
+
+
+<div>
+    <h4 align="center">
+        • <a href="https://arxiv.org/abs/2309.14137" target='_blank'>NeurIPS 2023</a> •
+    </h4>
+</div>
+
+[![KITTI Benchmark](https://img.shields.io/badge/KITTI%20Benchmark-2nd%20among%20all%20at%20submission%20time-blue)](https://www.cvlibs.net/datasets/kitti/eval_depth.php?benchmark=depth_prediction)
+[![Hugging Space Badge](https://img.shields.io/badge/🤗-Open%20In%20Spaces-blue.svg)](https://huggingface.co/spaces/umuthopeyildirim/IEBins-Depth-Estimation)
+
+## Abstract
+
+<div style="text-align:center">
+<img src="assets/teaser.jpg"  width="80%" height="80%">
+</div>
+
+</div>
+<strong>We propose a novel concept of iterative elastic bins for the classification-regression-based MDE. The proposed IEBins aims to search for high-quality depth by progressively optimizing the search range, which involves multiple stages and each stage performs a finer-grained depth search in the target bin on top of its previous stage. To alleviate the possible error accumulation during the iterative process, we utilize a novel elastic target bin to replace the original target bin, the width of which is adjusted elastically based on the depth uncertainty. </strong>
+
+---
+
+</div>
+
+## Installation
+```
+conda create -n iebins python=3.8
+conda activate iebins
+conda install pytorch=1.10.0 torchvision cudatoolkit=11.1
+pip install matplotlib, tqdm, tensorboardX, timm, mmcv, open3d
+```
+
+## Datasets
+You can prepare the datasets KITTI and NYUv2 according to [here](https://github.com/cleinc/bts/tree/master/pytorch) and download the SUN RGB-D dataset from [here](https://rgbd.cs.princeton.edu/), and then modify the data path in the config files to your dataset locations.
+
+
+## Training
+First download the pretrained encoder backbone from [here](https://github.com/microsoft/Swin-Transformer), and then modify the pretrain path in the config files. If you want to train the KITTI_Official model, first download the pretrained encoder backbone from [here](https://drive.google.com/file/d/1qjDnMwmEz0k0XWh7GP2aNPGiAjvOPF_5/view?usp=drive_link), which is provided by [MIM](https://github.com/SwinTransformer/MIM-Depth-Estimation).
+
+Training the NYUv2 model:
+```
+python iebins/train.py configs/arguments_train_nyu.txt
+```
+
+Training the KITTI_Eigen model:
+```
+python iebins/train.py configs/arguments_train_kittieigen.txt
+```
+
+Training the KITTI_Official model:
+```
+python iebins_kittiofficial/train.py configs/arguments_train_kittiofficial.txt
+```
+
+## Evaluation
+Evaluate the NYUv2 model:
+```
+python iebins/eval.py configs/arguments_eval_nyu.txt
+```
+
+Evaluate the NYUv2 model on the SUN RGB-D dataset:
+```
+python iebins/eval_sun.py configs/arguments_eval_sun.txt
+```
+
+Evaluate the KITTI_Eigen model:
+```
+python iebins/eval.py configs/arguments_eval_kittieigen.txt
+```
+
+To generate KITTI Online evaluation data for the KITTI_Official model:
+```
+python iebins_kittiofficial/test.py --data_path path to dataset --filenames_file ./data_splits/kitti_official_test.txt --max_depth 80 --checkpoint_path path to pretrained checkpoint  --dataset kitti --do_kb_crop
+```
+
+## Qualitative Depth and Point Cloud Results
+You can download the qualitative depth results of [IEBins](https://arxiv.org/abs/2309.14137), [NDDepth](https://arxiv.org/abs/2309.10592), [NeWCRFs](https://openaccess.thecvf.com/content/CVPR2022/html/Yuan_Neural_Window_Fully-Connected_CRFs_for_Monocular_Depth_Estimation_CVPR_2022_paper.html), [PixelFormer](https://openaccess.thecvf.com/content/WACV2023/html/Agarwal_Attention_Attention_Everywhere_Monocular_Depth_Prediction_With_Skip_Attention_WACV_2023_paper.html), [AdaBins](https://openaccess.thecvf.com/content/CVPR2021/html/Bhat_AdaBins_Depth_Estimation_Using_Adaptive_Bins_CVPR_2021_paper.html) and [BTS](https://arxiv.org/abs/1907.10326) on the test sets of NYUv2 and KITTI_Eigen from [here](https://pan.baidu.com/s/1zaFe40mwpQ5cvdDlLZRrCQ?pwd=vfxd) and download the qualitative point cloud results of IEBins, NDDepth, NeWCRFS, PixelFormer, AdaBins and BTS on the NYUv2 test set from [here](https://pan.baidu.com/s/1WwpFuPBGBUaSGPEdThJ6Rw?pwd=n9rw). 
+
+If you want to derive these results by yourself, please refer to the test.py.
+
+If you want to perform inference on a single image, run:
+```
+python iebins/inference_single_image.py --dataset kitti or nyu --image_path path to image --checkpoint_path path to pretrained checkpoint --max_depth 80 or 10
+```
+Then you can acquire the qualitative depth result.
+
+## Gradio Demo
+
+1. Install the gradio and other required libraries
+
+```python
+pip install gradio gradio_imageslider timm -q
+```
+
+2. Run the demo
+
+```python
+python notebooks/app.py
+```
+
+
+## Models
+| Model | Abs Rel | Sq Rel | RMSE | a1 | a2 | a3| Link|
+| ------------ | :---: | :---: | :---: |  :---: |  :---: |  :---: |  :---: |
+|NYUv2 (Swin-L)| 0.087 | 0.040 | 0.314 | 0.936 | 0.992 | 0.998 |[[Google]](https://drive.google.com/file/d/14Rn-vxvpXO2EXRaWqCPmh2JufvOurwtl/view?usp=drive_link) [[Baidu]](https://pan.baidu.com/s/1E2KAHtQ-ul99RGp_G7QK1w?pwd=7o4d)|
+|NYUv2 (Swin-T)| 0.108 | 0.061 | 0.375 | 0.893 | 0.984 | 0.996 |[[Google]](https://drive.google.com/file/d/1eYkTb3grbDitQ9tJdg1DhAOaGmqgHWHK/view?usp=drive_link) [[Baidu]](https://pan.baidu.com/s/1v5_MJtP0YOSoark9Yw1RaQ?pwd=2k5d)|
+|KITTI_Eigen (Swin-L)| 0.050 | 0.142 | 2.011 | 0.978 | 0.998 | 0.999 |[[Google]](https://drive.google.com/file/d/1xaVLDq7zJ-C2GtFvABolSUtK7gzvNQNd/view?usp=drive_link) [[Baidu]](https://pan.baidu.com/s/16mRrKrr9PdZhuZ3ZlkmNlA?pwd=lcjd)|
+|KITTI_Eigen (Swin-T)| 0.056 | 0.169 | 2.205 | 0.970 | 0.996 | 0.999 |[[Google]](https://drive.google.com/file/d/1s0LXZmS6_Q4_H_0hmbOldPcVhlRw8Dut/view?usp=drive_link) [[Baidu]](https://pan.baidu.com/s/1xgeqIX5WP5F2MFwypMWV5A?pwd=ygfi)|
+
+| Model | SILog| Abs Rel | Sq Rel | RMSE | a1 | a2 | a3| Link|
+| ------------ | :---: | :---: | :---: | :---: |  :---: |  :---: |  :---: |  :---: |
+|KITTI_Official (Swinv2-L)| 7.48 | 5.20 | 0.79 | 2.34 | 0.974 | 0.996 | 0.999 |[[Google]](https://drive.google.com/file/d/19ARBiDTIvtZSWJVvhbEWBcZMonXsiOX1/view?usp=drive_link)|
+
+
+## Citation
+
+If you find our work useful in your research please consider citing our paper:
+
+```
+@inproceedings{shao2023IEBins,
+title={IEBins: Iterative Elastic Bins for Monocular Depth Estimation},
+author={Shao, Shuwei and Pei, Zhongcai and Wu, Xingming and Liu, Zhong and Chen, Weihai and Li, Zhengguo},
+booktitle={Advances in Neural Information Processing Systems (NeurIPS)},
+year={2023}
+}
+```
+
+## Contact
+
+If you have any questions, please feel free to contact swshao@buaa.edu.cn.
+
+
+## Acknowledgement
+
+Our code is based on the implementation of [NeWCRFs](https://github.com/aliyun/NeWCRFs) and [BTS](https://github.com/cleinc/bts). We thank their excellent works.
---
+++ iebins/networks/NewCRFDepth.py
@@ -0,0 +1,318 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from .swin_transformer import SwinTransformer
+from .newcrf_layers import NewCRF
+from .uper_crf_head import PSP
+from .depth_update  import *
+########################################################################################################################
+
+
+class NewCRFDepth(nn.Module):
+    """
+    Depth network based on neural window FC-CRFs architecture.
+    """
+    def __init__(self, version=None, inv_depth=False, pretrained=None, 
+                    frozen_stages=-1, min_depth=0.1, max_depth=100.0, **kwargs):
+        super().__init__()
+
+        self.inv_depth = inv_depth
+        self.with_auxiliary_head = False
+        self.embed_dim = False
+
+        norm_cfg = dict(type='BN', requires_grad=True)
+
+        window_size = int(version[-2:])
+
+        if version[:-2] == 'base':
+            embed_dim = 128
+            depths = [2, 2, 18, 2]
+            num_heads = [4, 8, 16, 32]
+            in_channels = [128, 256, 512, 1024]
+            self.update = BasicUpdateBlockDepth(hidden_dim=128, context_dim=128)
+        elif version[:-2] == 'large':
+            embed_dim = 192
+            depths = [2, 2, 18, 2]
+            num_heads = [6, 12, 24, 48]
+            in_channels = [192, 384, 768, 1536]
+            self.update = BasicUpdateBlockDepth(hidden_dim=128, context_dim=192)
+        elif version[:-2] == 'tiny':
+            embed_dim = 96
+            depths = [2, 2, 6, 2]
+            num_heads = [3, 6, 12, 24]
+            in_channels = [96, 192, 384, 768]
+            self.update = BasicUpdateBlockDepth(hidden_dim=128, context_dim=96)
+
+        backbone_cfg = dict(
+            embed_dim=embed_dim,
+            depths=depths,
+            num_heads=num_heads,
+            window_size=window_size,
+            ape=False,
+            drop_path_rate=0.3,
+            patch_norm=True,
+            use_checkpoint=False,
+            frozen_stages=frozen_stages
+        )
+
+        embed_dim = 512
+        decoder_cfg = dict(
+            in_channels=in_channels,
+            in_index=[0, 1, 2, 3],
+            pool_scales=(1, 2, 3, 6),
+            channels=embed_dim,
+            dropout_ratio=0.0,
+            num_classes=32,
+            norm_cfg=norm_cfg,
+            align_corners=False
+        )
+
+        self.backbone = SwinTransformer(**backbone_cfg)
+        v_dim = decoder_cfg['num_classes']*4
+        win = 7
+        crf_dims = [128, 256, 512, 1024]
+        v_dims = [64, 128, 256, embed_dim]
+        self.crf3 = NewCRF(input_dim=in_channels[3], embed_dim=crf_dims[3], window_size=win, v_dim=v_dims[3], num_heads=32)
+        self.crf2 = NewCRF(input_dim=in_channels[2], embed_dim=crf_dims[2], window_size=win, v_dim=v_dims[2], num_heads=16)
+        self.crf1 = NewCRF(input_dim=in_channels[1], embed_dim=crf_dims[1], window_size=win, v_dim=v_dims[1], num_heads=8)
+
+        self.decoder = PSP(**decoder_cfg)
+        self.disp_head1 = DispHead(input_dim=crf_dims[0])
+
+        self.up_mode = 'bilinear'
+        if self.up_mode == 'mask':
+            self.mask_head = nn.Sequential(
+                nn.Conv2d(v_dims[0], 64, 3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(64, 16*9, 1, padding=0))
+
+        self.min_depth = min_depth
+        self.max_depth = max_depth
+        self.depth_num = 16
+        self.hidden_dim = 128
+        self.project = Projection(v_dims[0], self.hidden_dim)
+
+        self.init_weights(pretrained=pretrained)
+
+    def init_weights(self, pretrained=None):
+        """Initialize the weights in backbone and heads.
+
+        Args:
+            pretrained (str, optional): Path to pre-trained weights.
+                Defaults to None.
+        """
+        print(f'== Load encoder backbone from: {pretrained}')
+        self.backbone.init_weights(pretrained=pretrained)
+        self.decoder.init_weights()
+        if self.with_auxiliary_head:
+            if isinstance(self.auxiliary_head, nn.ModuleList):
+                for aux_head in self.auxiliary_head:
+                    aux_head.init_weights()
+            else:
+                self.auxiliary_head.init_weights()
+
+    def upsample_mask(self, disp, mask):
+        """ Upsample disp [H/4, W/4, 1] -> [H, W, 1] using convex combination """
+        N, C, H, W = disp.shape   #自定义的掩码和凸组合来实现高质量的上采样。
+        mask = mask.view(N, 1, 9, 4, 4, H, W)
+        mask = torch.softmax(mask, dim=2)
+
+        up_disp = F.unfold(disp, kernel_size=3, padding=1)
+        up_disp = up_disp.view(N, C, 9, 1, 1, H, W)
+
+        up_disp = torch.sum(mask * up_disp, dim=2)
+        up_disp = up_disp.permute(0, 1, 4, 2, 5, 3)
+        return up_disp.reshape(N, C, 4*H, 4*W)
+
+    def forward(self, imgs, epoch=1, step=100):
+
+        feats = self.backbone(imgs) #swintransformer,输出四级特征金字塔
+        ppm_out = self.decoder(feats) #psp
+
+        e3 = self.crf3(feats[3], ppm_out)  #每级特征都要
+        e3 = nn.PixelShuffle(2)(e3) #像素洗牌，最高像素为（h/4 * w/4）通常用于将深度学习模型中的高分辨率特征图上采样到更高的分辨率
+        e2 = self.crf2(feats[2], e3)
+        e2 = nn.PixelShuffle(2)(e2)
+        e1 = self.crf1(feats[1], e2)
+        e1 = nn.PixelShuffle(2)(e1) #输出结果分别作为上下文特征和隐藏状态的初始化发给iebins
+        
+        # iterative bins
+        if epoch == 0 and step < 80:
+            max_tree_depth = 3
+        else:
+            max_tree_depth = 6
+        
+        if self.up_mode == 'mask':
+            mask = self.mask_head(e1)
+
+        b, c, h, w = e1.shape
+        device = e1.device
+               
+        depth = torch.zeros([b, 1, h, w]).to(device)
+        context = feats[0]
+        gru_hidden = torch.tanh(self.project(e1))
+        pred_depths_r_list, pred_depths_c_list, uncertainty_maps_list = self.update(depth, context, gru_hidden, max_tree_depth, self.depth_num, self.min_depth, self.max_depth)
+        
+        if self.up_mode == 'mask':
+            for i in range(len(pred_depths_r_list)):
+                pred_depths_r_list[i] = self.upsample_mask(pred_depths_r_list[i], mask)  
+            for i in range(len(pred_depths_c_list)):
+                pred_depths_c_list[i] = self.upsample_mask(pred_depths_c_list[i], mask.detach())
+            for i in range(len(uncertainty_maps_list)):
+                uncertainty_maps_list[i] = self.upsample_mask(uncertainty_maps_list[i], mask.detach())                   
+        else:
+            for i in range(len(pred_depths_r_list)):
+                pred_depths_r_list[i] = upsample(pred_depths_r_list[i], scale_factor=4)
+            for i in range(len(pred_depths_c_list)):
+                pred_depths_c_list[i] = upsample(pred_depths_c_list[i], scale_factor=4) 
+            for i in range(len(uncertainty_maps_list)):
+                uncertainty_maps_list[i] = upsample(uncertainty_maps_list[i], scale_factor=4) 
+
+        return pred_depths_r_list, pred_depths_c_list, uncertainty_maps_list
+
+class DispHead(nn.Module):
+    def __init__(self, input_dim=100):
+        super(DispHead, self).__init__()
+        # self.norm1 = nn.BatchNorm2d(input_dim)
+        self.conv1 = nn.Conv2d(input_dim, 1, 3, padding=1)
+        # self.relu = nn.ReLU(inplace=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x, scale):
+        # x = self.relu(self.norm1(x))
+        x = self.sigmoid(self.conv1(x))
+        if scale > 1:
+            x = upsample(x, scale_factor=scale)
+        return x
+
+class BasicUpdateBlockDepth(nn.Module):
+    def __init__(self, hidden_dim=128, context_dim=192):
+        super(BasicUpdateBlockDepth, self).__init__()
+                
+        self.encoder = ProjectionInputDepth(hidden_dim=hidden_dim, out_chs=hidden_dim * 2)#深度候选D投影到特征空间中
+        self.gru = SepConvGRU(hidden_dim=hidden_dim, input_dim=self.encoder.out_chs+context_dim)
+        self.p_head = PHead(hidden_dim, hidden_dim) #更新隐藏状态预测
+
+    def forward(self, depth, context, gru_hidden, seq_len, depth_num, min_depth, max_depth):
+ 
+        pred_depths_r_list = [] #用于存储右侧视图的预测深度。
+        pred_depths_c_list = [] #用于存储中心视图的预测深度。
+        uncertainty_maps_list = [] #用于存储不确定性图，这可能表示预测深度的不确定性
+      
+        b, _, h, w = depth.shape  #获取batchsize height
+        depth_range = max_depth - min_depth #深度范围
+        interval = depth_range / depth_num  #深度间隔  width
+        interval = interval * torch.ones_like(depth)  #创建一个和depth一样的图数值全为interval
+        interval = interval.repeat(1, depth_num, 1, 1)  #通道重复depth_num次
+        interval = torch.cat([torch.ones_like(depth) * min_depth, interval], 1) #将两个组合
+
+        bin_edges = torch.cumsum(interval, 1) #累加箱边缘
+        current_depths = 0.5 * (bin_edges[:, :-1] + bin_edges[:, 1:])  #(a(n)+a(n+1))/2 depth candidate
+        index_iter = 0  #迭代系数
+
+        for i in range(seq_len):
+            input_features = self.encoder(current_depths.detach() ) #利用深度候选获取每个像素的概率分布
+            input_c = torch.cat([input_features, context], dim=1) #输入特征和上下文特征组合
+
+            gru_hidden = self.gru(gru_hidden, input_c)#投影特征和上下文特征连接起来作为gru输入
+            pred_prob = self.p_head(gru_hidden) #深度概率 pn(p)
+
+            depth_r = (pred_prob * current_depths.detach()).sum(1, keepdim=True) #深度候选线性组合
+            pred_depths_r_list.append(depth_r) #深度值
+
+            uncertainty_map = torch.sqrt((pred_prob * ((current_depths.detach() - depth_r.repeat(1, depth_num, 1, 1))**2)).sum(1, keepdim=True)) #深度不准确性的标准差
+            uncertainty_maps_list.append(uncertainty_map) #深度不确定方差列表
+        
+            index_iter = index_iter + 1
+
+            pred_label = get_label(torch.squeeze(depth_r, 1), bin_edges, depth_num).unsqueeze(1)
+            depth_c = torch.gather(current_depths.detach(), 1, pred_label.detach())
+            pred_depths_c_list.append(depth_c)
+
+            label_target_bin_left = pred_label
+            target_bin_left = torch.gather(bin_edges, 1, label_target_bin_left)
+            label_target_bin_right = (pred_label.float() + 1).long()
+            target_bin_right = torch.gather(bin_edges, 1, label_target_bin_right)
+            #更新深度候选
+            bin_edges, current_depths = update_sample(bin_edges, target_bin_left, target_bin_right, depth_r.detach(), pred_label.detach(), depth_num, min_depth, max_depth, uncertainty_map)
+
+        return pred_depths_r_list, pred_depths_c_list, uncertainty_maps_list
+
+class PHead(nn.Module):
+    def __init__(self, input_dim=128, hidden_dim=128):
+        super(PHead, self).__init__()
+        self.conv1 = nn.Conv2d(input_dim, hidden_dim, 3, padding=1)
+        self.conv2 = nn.Conv2d(hidden_dim, 16, 3, padding=1)
+    
+    def forward(self, x):
+        out = torch.softmax(self.conv2(F.relu(self.conv1(x))), 1)
+        return out
+
+class SepConvGRU(nn.Module):
+    def __init__(self, hidden_dim=128, input_dim=128+192):
+        super(SepConvGRU, self).__init__()
+
+        self.convz1 = nn.Conv2d(hidden_dim+input_dim, hidden_dim, (1,5), padding=(0,2))
+        self.convr1 = nn.Conv2d(hidden_dim+input_dim, hidden_dim, (1,5), padding=(0,2))
+        self.convq1 = nn.Conv2d(hidden_dim+input_dim, hidden_dim, (1,5), padding=(0,2))
+        self.convz2 = nn.Conv2d(hidden_dim+input_dim, hidden_dim, (5,1), padding=(2,0))
+        self.convr2 = nn.Conv2d(hidden_dim+input_dim, hidden_dim, (5,1), padding=(2,0))
+        self.convq2 = nn.Conv2d(hidden_dim+input_dim, hidden_dim, (5,1), padding=(2,0))
+
+    def forward(self, h, x):
+        # horizontal
+        hx = torch.cat([h, x], dim=1)
+        z = torch.sigmoid(self.convz1(hx)) #更新门
+        r = torch.sigmoid(self.convr1(hx)) #重置门
+        q = torch.tanh(self.convq1(torch.cat([r*h, x], dim=1))) 
+        
+        h = (1-z) * h + z * q
+
+        # vertical
+        hx = torch.cat([h, x], dim=1)
+        z = torch.sigmoid(self.convz2(hx))
+        r = torch.sigmoid(self.convr2(hx))
+        q = torch.tanh(self.convq2(torch.cat([r*h, x], dim=1)))       
+        h = (1-z) * h + z * q
+
+        return h
+
+class ProjectionInputDepth(nn.Module):
+    def __init__(self, hidden_dim, out_chs):
+        super().__init__()
+        self.out_chs = out_chs 
+        self.convd1 = nn.Conv2d(16, hidden_dim, 7, padding=3)
+        self.convd2 = nn.Conv2d(hidden_dim, hidden_dim, 3, padding=1)
+        self.convd3 = nn.Conv2d(hidden_dim, hidden_dim, 3, padding=1)
+        self.convd4 = nn.Conv2d(hidden_dim, out_chs, 3, padding=1)
+        
+    def forward(self, depth):
+        d = F.relu(self.convd1(depth))
+        d = F.relu(self.convd2(d))
+        d = F.relu(self.convd3(d))
+        d = F.relu(self.convd4(d))
+                
+        return d
+
+class Projection(nn.Module):
+    def __init__(self, in_chs, out_chs):
+        super().__init__()
+        self.conv = nn.Conv2d(in_chs, out_chs, 3, padding=1)
+        
+    def forward(self, x):
+        out = self.conv(x)
+                
+        return out
+
+def upsample(x, scale_factor=2, mode="bilinear", align_corners=False):
+    """Upsample input tensor by a factor of 2
+    """
+    return F.interpolate(x, scale_factor=scale_factor, mode=mode, align_corners=align_corners)
+
+def upsample1(x, scale_factor=2, mode="bilinear"):
+    """Upsample input tensor by a factor of 2
+    """
+    return F.interpolate(x, scale_factor=scale_factor, mode=mode)
+
+
